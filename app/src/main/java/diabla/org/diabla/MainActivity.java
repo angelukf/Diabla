6319246c217b55748fc4c8971a9a37f541a3ddb6
angel <angelukf@gmial.com>
2018-02-08 17:42:43 -0600

    el tercero

--- app/src/main/java/diabla/org/diabla/MainActivity.java
+++ app/src/main/java/diabla/org/diabla/MainActivity.java
@@ -2,6 +2,8 @@ package diabla.org.diabla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //malditoviruscxsvwdfvsdvsdfvcASCASDCASD
+          findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Toast.makeText(getApplicationContext(),"tu mensaje se envio ",Toast.LENGTH_LONG).show();
+              }
+          });
     }
 }
